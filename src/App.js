985bb42aed696e5ae5a9b44c7e86a260df86b3c1
Hanif <hanif.hakim@spesolution.com>
2022-07-19 22:02:32 +0700

    Front End Login & Register

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ import { ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
 import theme from "./themes/theme";
 import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/" element={<Homepage />} />
           <Route path="/movie/:id" element={<DetailMovie />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </ThemeProvider>
     </>
   );
